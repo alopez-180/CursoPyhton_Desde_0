d94e0a1c36a9a1f972ce441662ed7e2bb7cadfb8
Alex Lopez <alopez@ifr.es>
2025-01-21 15:14:48 +0100

    Cambios 07_dicts.py

--- 07_dicts.py
+++ 07_dicts.py
@@ -29,5 +29,25 @@ print(my_other_dict)
 
 ######################################
 
-print("Alex" in my_other_dict) # Esta operación dara False, porque realmente "Alex" existe como valor y no como clave.
+print("Pedro" in my_other_dict) # Esta operación dara False, porque realmente "Pedro" existe como valor y no como clave.
+print("Pedro" in my_other_dict["Nombre"]) # Esta operación dara True, porque dentro de la clavo Nombre, si que existe el elemento "Pedro"
 
+print("#################")
+
+print(my_other_dict.items()) # Nos ordena por lista la clave y el valors
+print(my_other_dict.keys()) # Nos devuelve unicamente las claves que tenemos
+print(my_other_dict.values()) # Nos devuelve los elementos que tenemos
+
+
+my_other_dict = (dict.fromkeys(("Nombre", 1))) # El "fromkeys" lo que hace es crear claves con valores vacios 
+print(my_other_dict)
+
+my_other_dict = ["Nombre", 1, "Apellido"]
+my_other_dict = (dict.fromkeys(my_other_dict))
+print(my_other_dict)
+
+my_other_dict["Nombre"] = "Alex" , "Pepe"
+print(my_other_dict)
+
+my_other_dict["Nombre"] = "Alex" , "Pepe"
+print(my_other_dict)
