32cd8083f584aa9ea27d24970ab2cde082d58e5d
Alex Lopez <alopez@ifr.es>
2025-01-28 13:07:17 +0100

    Restructuración archivos + Ejercicios para practicar

---
+++ Ejercicios_PracticarLogica/01_FizzBuzz.py
@@ -0,0 +1,22 @@
+"""
+/*
+ * Escribe un programa que muestre por consola (con un print) los
+ * números de 1 a 100 (ambos incluidos y con un salto de línea entre
+ * cada impresión), sustituyendo los siguientes:
+ * - Múltiplos de 3 por la palabra "fizz".
+ * - Múltiplos de 5 por la palabra "buzz".
+ * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
+ */
+ """
+
+# El operante % nos sirve para calcular el resto de una division, si el resto es 0 quiere decir que el numero es multiplo.
+
+for i in range(1,101):
+    if (i % 3 == 0 and i % 5 == 0):
+        print("fizzbuzz")
+    elif (i % 3 == 0):
+        print ("fizz")
+    elif (i % 5 == 0):
+        print("buzz")
+    else:
+        print(i)
--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-for i in range(tamaño):
-    for j in range(tamaño):
-        if  j == 0 or j == tamaño - 1 or i == (tamaño // 2):
-            print("*", end=" ")
-        else:
-            print(" ", end=" ")
-    print()
